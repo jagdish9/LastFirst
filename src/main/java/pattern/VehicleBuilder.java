f4d885d7505528b6f061602a5cb7a79058518e0c
jkaiwari <jagdish.kaiwaritya@oracle.com>
2021-02-23 09:06:58 +0530

    To see blame

--- src/main/java/pattern/VehicleBuilder.java
+++ src/main/java/pattern/VehicleBuilder.java
@@ -1,3 +1,32 @@
+/*
+ *  This source is part of the FLEXCUBE Software Product.
+ *  Copyright (c) 2008 ,2020, Oracle and/or its affiliates.
+ *  All rights reserved.
+ *
+ *  No part of this work may be reproduced, stored in a retrieval system,
+ *  adopted or transmitted in any form or by any means, electronic, mechanical, photographic,
+ *  graphic, optic recording or otherwise, translated in any language or computer language,
+ *  without the prior written permission of Oracle and/or its affiliates.
+ *
+ *  Oracle Financial Services Software Limited.
+ *  Oracle Park, Off Western Express Highway,
+ *  Goregaon (East),
+ *  Mumbai - 400 063,
+ *  India.
+ *
+ *  Written by         : Pankaj Chaudhary
+ *  Date of creation   : 04-12-2020
+ *  File Name          : SmartConstant.java
+ *  Purpose			  : Constant declaration for SMART.
+ *  Called From       :
+ *
+ *  CHANGE LOG
+ *  Last Modified By   :
+ *  Last modified on   :
+ *  Full Version       :
+ *  Reason             :
+ */
+
 package pattern;
 
 import java.util.ArrayList;
---
+++ src/main/java/scqs/CopyrightHolder.java
@@ -0,0 +1,37 @@
+/*
+ *  This source is part of the FLEXCUBE Software Product.
+ *  Copyright (c) 2008 ,2020, Oracle and/or its affiliates.
+ *  All rights reserved.
+ *
+ *  No part of this work may be reproduced, stored in a retrieval system,
+ *  adopted or transmitted in any form or by any means, electronic, mechanical, photographic,
+ *  graphic, optic recording or otherwise, translated in any language or computer language,
+ *  without the prior written permission of Oracle and/or its affiliates.
+ *
+ *  Oracle Financial Services Software Limited.
+ *  Oracle Park, Off Western Express Highway,
+ *  Goregaon (East),
+ *  Mumbai - 400 063,
+ *  India.
+ *
+ *  Written by         :
+ *  Date of creation   :
+ *  File Name          :
+ *  Purpose            :
+ *  Called From        :
+ *
+ *  CHANGE LOG
+ *  Last Modified By   :
+ *  Last modified on   :
+ *  Full Version       :
+ *  Reason             :
+ */
+
+package scqs;
+
+public class CopyrightHolder {
+
+    public static void main(String[] args) {
+
+    }
+}
